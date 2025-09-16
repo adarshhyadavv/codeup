c7cca7ad0ec5cf54acac650a92ac42ccb803fb18
ADARSH YADAV <adarshyadav@ADARSHs-MacBook-Air.local>
2025-09-17 00:59:26 +0530

    readability improved

--- assignment1/task5.java
+++ assignment1/task5.java
@@ -13,22 +13,19 @@ public class task5 {
     public static int lengthOfLongestSubstring(String s) {
         int n = s.length();
         int maxLength = 0;
-        int left = 0; // left pointer of sliding window
+        int left = 0; 
         HashSet<Character> set = new HashSet<>();
 
         for (int right = 0; right < n; right++) {
             char c = s.charAt(right);
 
-            // Shrink window if duplicate is found
             while (set.contains(c)) {
                 set.remove(s.charAt(left));
                 left++;
             }
 
-            // Add current character to set
             set.add(c);
 
-            // Update max length
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
